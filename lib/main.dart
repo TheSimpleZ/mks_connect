50096e729715d1a8cb03c2604696c393c1a6b7dd
Zrean Tofiq <mote93@gmail.com>
2022-08-16 01:46:06 +0200

    Working example

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mks_connect/mks_client.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    const title = 'MKS Connect';
+    return const MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
 
   final String title;
 
@@ -48,68 +31,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _controller = TextEditingController();
+  final _channel = MKSClient('ws://dodohub.eu:7000');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            StreamBuilder(
+              stream: _channel.bedTemp,
+              builder: (context, snapshot) {
+                final temp = snapshot.hasData ? "${snapshot.data}" : 'N/A';
+                return Text("Bed temperature: $temp");
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            StreamBuilder(
+              stream: _channel.t0Temp,
+              builder: (context, snapshot) {
+                final temp = snapshot.hasData ? "${snapshot.data}" : 'N/A';
+                return Text("Extruder 1 temperature: $temp");
+              },
+            ),
+            StreamBuilder(
+              stream: _channel.t1Temp,
+              builder: (context, snapshot) {
+                final temp = snapshot.hasData ? "${snapshot.data}" : 'N/A';
+                return Text("Extruder 2 temperature: $temp");
+              },
+            ),
+            FutureBuilder(
+              future: _channel.sdCardFiles,
+              builder: ((context, snapshot) {
+                var txt = snapshot.hasData
+                    ? (snapshot.data as List<String>).join('\n')
+                    : 'N/A';
+
+                return Text("SD card files:\n $txt");
+              }),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // void _sendMessage() {
+  //   if (_controller.text.isNotEmpty) {
+  //     print("Sending message: ${_controller.text}");
+  //     _channel.sink.add(utf8.encode('${_controller.text}\n'));
+  //   }
+  // }
+
+  @override
+  void dispose() {
+    // _channel.sink.close();
+    _channel.dispose();
+    _controller.dispose();
+    super.dispose();
   }
 }
---
+++ lib/mks_client.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:async/async.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+final temperatureReadingPattern = RegExp(
+    r"T:\d+ /0 B:(?<bed>\d+) /0 T0:(?<t0>\d+) /0 T1:(?<t1>\d+) /0 @:\d+ B@:\d+");
+
+abstract class MSKCommands {
+  static String ListFiles = "M20";
+}
+
+abstract class MSKStates {
+  static String idle = "IDLE";
+  static String printing = "PRINTING";
+  static String pause = "PAUSE";
+}
+
+class MKSClient {
+  final WebSocketChannel _channel;
+
+  late final _parsedStream = _channel.stream
+      .asBroadcastStream()
+      .cast<List<int>>()
+      .transform(utf8.decoder)
+      .transform(const LineSplitter());
+
+  late final _temperatureReadings =
+      _parsedStream.where((line) => temperatureReadingPattern.hasMatch(line));
+
+  Stream<int> _parseTemp(String groupName) =>
+      _temperatureReadings.map((line) => int.parse(
+          temperatureReadingPattern.firstMatch(line)!.namedGroup(groupName)!));
+
+  late final bedTemp = _parseTemp("bed");
+  late final t0Temp = _parseTemp("t0");
+  late final t1Temp = _parseTemp("t1");
+
+  MKSClient(String uri)
+      : _channel = WebSocketChannel.connect(
+          Uri.parse(uri),
+        ) {
+    _parsedStream.listen(print);
+  }
+
+  void dispose() {
+    _channel.sink.close();
+  }
+
+  sendCommand(String command) {
+    print("Sending command: $command");
+    _channel.sink.add(utf8.encode('$command\n'));
+  }
+
+  Future<List<String>> get sdCardFiles async {
+    final events = StreamQueue(_parsedStream);
+    sendCommand(MSKCommands.ListFiles);
+
+    while (true) {
+      var nextLine = await events.next;
+      if (nextLine.startsWith('Begin file list')) {
+        await events.next; // Discard system volume info
+        break;
+      }
+    }
+
+    final files = <String>[];
+    while (true) {
+      final line = await events.next;
+      if (line.startsWith('End file list')) {
+        break;
+      }
+      files.add(line);
+    }
+    events.cancel();
+    return files;
+  }
+}
